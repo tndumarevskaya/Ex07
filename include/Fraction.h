aa4a0c4507cc220626ff76471076284f87cb962b
tndumarevskaya <dumtanya2009@gmail.com>
2020-12-05 16:28:02 +0300

    fiiix

--- include/Fraction.h
+++ include/Fraction.h
@@ -4,25 +4,27 @@
 
 #include <iostream>
 #include <cstring>
+#include <string>
 
-class Fraction
-{
+class Fraction {
  private:
   int numerator;
   int denominator;
  public:
-  explicit Fraction(int num = 0, int den = 1):numerator{num}, denominator{den}{}; //конструктор с двумя параметрами, со значениями по - умолчанию(0, 1).
-  Fraction(const Fraction& new_f) :numerator{ new_f.numerator }, denominator{ new_f.denominator }{};//конструктор копирования.
-  std::string getValue(); //метод, возвращающий строку типа std::string, содержащую изображение дроби, например "1/2" или "-5/6".Дробь с единичным знаменателем выводится как целое число.
-  int getNumerator(); //вернуть числитель.
-  int getDenominator(); //вернуть знаменатель.
-  Fraction operator+(const Fraction& frac); //сложение
-  Fraction operator-(const Fraction& frac); //вычитание
-  Fraction operator*(const Fraction& frac); //умножение
-  Fraction operator/(const Fraction& frac); //деление
-  Fraction& operator=(const Fraction& frac); //присваивание
+  explicit Fraction(int num = 0, int den = 1):
+    numerator{num}, denominator{den}{};
+  Fraction(const Fraction& new_f):
+    numerator{ new_f.numerator }, denominator{ new_f.denominator }{};
+  std::string getValue();
+  int getNumerator();
+  int getDenominator();
+  Fraction operator+(const Fraction& frac);
+  Fraction operator-(const Fraction& frac);
+  Fraction operator*(const Fraction& frac);
+  Fraction operator/(const Fraction& frac);
+  Fraction& operator=(const Fraction& frac);
  protected:
-  Fraction normalize(); //закрытый метод нормировки(сокращения дроби 2 / 4 -> 1 / 2).
+  Fraction normalize();
 };
 
-#endif // INCLUDE_FRACTION_H_
+#endif  // INCLUDE_FRACTION_H_
--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,6 +1,5 @@
 // Copyright 2020 Dumarevskaya
 #include "Fraction.h"
-#include <string>
 
 std::string Fraction::getValue() {
   std::string str;
@@ -16,68 +15,68 @@ std::string Fraction::getValue() {
     }
   }
   return str;
-};
+}
 
 int Fraction::getNumerator() {
   *this = this->normalize();
   return numerator;
-};
+}
+
 int Fraction::getDenominator() {
   *this = this->normalize();
   return denominator;
-};
+}
 
-Fraction Fraction::operator+(const Fraction& frac) {
-  if (denominator == frac.denominator) {
-    Fraction new_f(numerator + frac.numerator, denominator);
+Fraction Fraction::operator+(const Fraction& fr) {
+  if (denominator == fr.denominator) {
+    Fraction new_f(numerator + fr.numerator, denominator);
     return new_f;
   }
   else {
     Fraction new_f;
-    new_f.numerator = numerator * frac.denominator + denominator * frac.numerator;
-    new_f.denominator = denominator * frac.denominator;
+    new_f.numerator = numerator * fr.denominator + denominator * fr.numerator;
+    new_f.denominator = denominator * fr.denominator;
     return new_f;
   }
-};
+}
 
-Fraction Fraction::operator-(const Fraction& frac) {
-  if (denominator == frac.denominator) {
-    Fraction new_f(numerator - frac.numerator, denominator);
+Fraction Fraction::operator-(const Fraction& fr) {
+  if (denominator == fr.denominator) {
+    Fraction new_f(numerator - fr.numerator, denominator);
     return new_f;
   }
   else {
     Fraction new_f;
-    new_f.numerator = numerator * frac.denominator - denominator * frac.numerator;
-    new_f.denominator = denominator * frac.denominator;
+    new_f.numerator = numerator * fr.denominator - denominator * fr.numerator;
+    new_f.denominator = denominator * fr.denominator;
     return new_f;
   }
-};
+}
 
 Fraction Fraction::operator*(const Fraction& frac) {
   return Fraction(numerator * frac.numerator, denominator * frac.denominator);
 };
 
-Fraction Fraction::operator/(const Fraction& frac) {
-  try { //ищем исключени€, которые выбрасываютс€ в блоке try, и отправл€ем их дл€ обработки в блок(и) catch
-    if (frac.numerator == 0)
+Fraction Fraction::operator/(const Fraction& fr) {
+  try {
+    if (fr.numerator == 0)
       throw 0;
-      return Fraction(numerator * frac.denominator, denominator * frac.numerator);
+    return Fraction(numerator * fr.denominator, denominator * fr.numerator);
   }
-  catch (int) { // обработка исключений типа int
+  catch (int) {
     denominator = 0;
   }
   return *this;
-};
+}
 
 Fraction& Fraction::operator=(const Fraction& frac) {
-  //проверка на самоприсваивание
   if (this == &frac) {
     return *this;
   }
   numerator = frac.numerator;
   denominator = frac.denominator;
   return *this;
-};
+}
 
 Fraction Fraction::normalize() {
   int min = numerator, max = denominator;
@@ -93,4 +92,4 @@ Fraction Fraction::normalize() {
   numerator = numerator / min;
   denominator = denominator / min;
   return *this;
-};
+}
