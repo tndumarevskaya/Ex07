7a6ee91731225555088243002a428cadafab2142
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-12-05 17:06:06 +0300

    ...

--- include/Fraction.h
+++ include/Fraction.h
@@ -1,5 +1,5 @@
 //  Copyright 2020 Dumarevskaya.
-#ifndef INCLUDE_FRACTION_H_
+#ifndef INCLUDE_FRACTION_H
 #define INCLUDE_FRACTION_H_
 
 #include <iostream>
@@ -7,25 +7,24 @@
 #include <string>
 
 class Fraction {
-private:
-    int numerator;
-    int denominator;
-public:
-    explicit Fraction(int num = 0, int den = 1) :
-        numerator{ num }, denominator{ den }{};
-    Fraction(const Fraction& new_f) :
-        numerator{ new_f.numerator }, denominator{ new_f.denominator }{};
-    std::string getValue();
-    int getNumerator();
-    int getDenominator();
-    Fraction operator+(const Fraction& frac);
-    Fraction operator-(const Fraction& frac);
-    Fraction operator*(const Fraction& frac);
-    Fraction operator/(const Fraction& frac);
-    Fraction& operator=(const Fraction& frac);
-protected:
-    Fraction normalize();
+ private:
+  int numerator;
+  int denominator;
+ public:
+  explicit Fraction(int num = 0, int den = 1):
+    numerator{num}, denominator{den}{};
+  Fraction(const Fraction& new_f):
+    numerator{ new_f.numerator }, denominator{ new_f.denominator }{};
+  std::string getValue();
+  int getNumerator();
+  int getDenominator();
+  Fraction operator+(const Fraction& frac);
+  Fraction operator-(const Fraction& frac);
+  Fraction operator*(const Fraction& frac);
+  Fraction operator/(const Fraction& frac);
+  Fraction& operator=(const Fraction& frac);
+ protected:
+  Fraction normalize();
 };
 
-#endif // !INCLUDE_FRACTION_H_
-
+#endif  // INCLUDE_FRACTION_H_
